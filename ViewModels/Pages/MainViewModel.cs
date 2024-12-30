3af0030673fdc90fbba92100dfa866fdb52dfe3e
xueaaaa <ss.bazunov@gmail.com>
2024-12-30 23:13:07 +0300

    working about task design

--- ViewModels/Pages/MainViewModel.cs
+++ ViewModels/Pages/MainViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using Ping.Models.Task;
-using System.Windows.Media;
 using Wpf.Ui.Controls;
+using Task = Ping.Models.Task.Task;
 
 namespace Ping.ViewModels.Pages
 {
     public partial class MainViewModel : ObservableObject, INavigationAware
     {
+        [ObservableProperty]
+        private List<Task> _tasks;
         [ObservableProperty]
         private List<Tag> _tags;
 
@@ -16,7 +18,15 @@ namespace Ping.ViewModels.Pages
 
         public void OnNavigatedTo()
         {
-
+            Tasks = new();
+            Tasks.Add(new Task()
+            {
+                Name = "Text",
+                Description = "Text",
+                LeadTime = DateTime.Now,
+                IsCompleted = false,
+                Priority = Priority.High
+            });
         }
     }
 }
