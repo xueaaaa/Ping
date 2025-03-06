fe9028656b0c050f8eb99d804cee55ae2acc3bd0
xueaaaa <ss.bazunov@gmail.com>
2025-03-06 22:24:26 +0300

    add: json writer/reader

---
+++ Models/StorageManager.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Text.Json;
+
+namespace Ping.Models
+{
+    internal static class StorageManager
+    {
+        private const string STORAGE_PATH = "storage";
+
+        public static async void SaveAsJson<T>(T obj, string filename)
+        {
+            string json = JsonSerializer.Serialize(obj);
+
+            using (StreamWriter sw = new($"{Directory.GetCurrentDirectory()}\\{STORAGE_PATH}\\{filename}.json"))
+            {
+                await sw.WriteAsync(json);
+            }
+        }
+
+        public static async Task<T?> ReadFromJson<T>(string filename)
+        {
+            using (StreamReader sr = new($"{Directory.GetCurrentDirectory()}\\{STORAGE_PATH}\\{filename}.json"))
+            {
+                string json = await sr.ReadToEndAsync();
+                T? obj = JsonSerializer.Deserialize<T>(json);
+
+                return obj;
+            }
+        }
+
+        public static async Task<List<T?>> ReadFromJson<T>(string[] filenames)
+        {
+            List<T?> list = new();
+
+            foreach (string filename in filenames)
+                list.Add(await ReadFromJson<T>(filename));
+
+            return list;
+        }
+    }
+}
